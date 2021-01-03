d5a0fd891921acdbd92aa2f166a87e1e1d442179
Rafael William <rafael.fim@hotmail.com>
2021-01-03 13:49:34 -0300

    connecting with the database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb://localhost/links", { useNewUrlParser: true, useUnifiedTopology: true }, (error, db) => {
+        console.log(error);
+        console.log(db);
+})
+
+
+
 
 app.get("/", (req, res) => {
         res.send("Hello World!");
